ed2568d7e78805bb4aecaf259b061fb9821e4c51
Dimitris Papazoglou <dmpapazog@gmail.com>
2018-12-03 18:34:19 +0200

    minor fixes

--- Bubble_sort.py
+++ Bubble_sort.py
@@ -5,6 +5,24 @@ import os
 SIZE = 20
 
 
+def print_array(pinakas, n):
+    print("[", end='')
+    for i in range(n - 1):
+        print("%3d" % pinakas[i], end=",")
+    print("%3d]" % pinakas[n-1])
+
+
+def bubble_sort(pinakas, n):
+    for i in range(n):
+        for j in range(n-1, i, -1):
+            if pinakas[j] < pinakas[j-1]:
+                pinakas[j], pinakas[j-1] = pinakas[j-1], pinakas[j]
+        print("Bhma %2d" % (i), end=" : ")
+        print_array(pinakas, n)
+
+    return pinakas
+
+
 def main():
     pinakas = []
     for i in range(SIZE):
@@ -14,30 +32,13 @@ def main():
 
     pinakas = bubble_sort(pinakas, SIZE)
     space = ' '
-    print("O neos pinakas." + '\n' + (10 * space), end = '')
+    print("O neos pinakas.\n" + (10 * space), end='')
     print_array(pinakas, SIZE)
 
     print()
     os.system("pause")
 
     return 0
-
-
-def print_array(pinakas, n):
-    print("[", end='')
-    for i in range(n - 1):
-        print("%3d"%pinakas[i], end=",")
-    print("%3d]"%pinakas[n-1])
-
-def bubble_sort(pinakas, n):
-    for i in range(n):
-        for j in range(n-1, i, -1):
-            if pinakas[j] < pinakas[j-1]:
-                pinakas[j], pinakas[j-1] = pinakas[j-1], pinakas[j]
-        print("Bhma %2d"%(i), end = " : ")
-        print_array(pinakas, n)
-
-    return pinakas
 
 
 if __name__ == "__main__":
