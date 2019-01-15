915e3a0e6f5474ef20df1760ab594a68945667e3
dmpapazog <dmpapazog@gmail.com>
2019-01-15 12:44:47 +0200

    refactored

--- Bubble_sort.py
+++ Bubble_sort.py
@@ -1,39 +1,45 @@
 import random
 import os
 
+SIZE = 10
 
-SIZE = 20
 
-
-def print_array(pinakas, n):
+def print_array(pinakas):
+    size = len(pinakas)
     print("[", end='')
-    for i in range(n - 1):
+    for i in range(size - 1):
         print("%3d" % pinakas[i], end=",")
-    print("%3d]" % pinakas[n-1])
+    print("%3d]" % pinakas[size-1])
 
 
-def bubble_sort(pinakas, n):
-    for i in range(n):
-        for j in range(n-1, i, -1):
+def bubble_sort(pinakas):
+    size = len(pinakas)
+    for i in range(size):
+        for j in range(size-1, i, -1):
             if pinakas[j] < pinakas[j-1]:
                 pinakas[j], pinakas[j-1] = pinakas[j-1], pinakas[j]
-        print("Bhma %2d" % (i), end=" : ")
-        print_array(pinakas, n)
+        print("Bhma %2d" % (i+1), end=" : ")
+        print_array(pinakas)
 
+
+def array_init(size):
+    pinakas = []
+    for i in range(size):
+        pinakas.append(random.randint(0, 100))
     return pinakas
 
 
 def main():
-    pinakas = []
-    for i in range(SIZE):
-        pinakas.append(random.randint(0, 100))
-    print("Arxikos pinakas.")
-    print_array(pinakas, SIZE)
+    pinakas = array_init(SIZE)
 
-    pinakas = bubble_sort(pinakas, SIZE)
+    print("Arxikos pinakas.")
+    print_array(pinakas)
+
+    bubble_sort(pinakas)
+
     space = ' '
-    print("O neos pinakas.\n" + (10 * space), end='')
-    print_array(pinakas, SIZE)
+    print("\nO neos pinakas.\n" + (10 * space), end='')
+    print_array(pinakas)
 
     print()
     os.system("pause")
